3cc2cf9d60d3621b806d4455b9e6dbc0751fc615
diogo-meneses-franca <diogo.meneses.franca@gmail.com>
2023-02-10 11:25:56 -0300

    Adicionado o arquivo do primeiro módulo

---
+++ eggplant-brownie/eggplant-brownie/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  eggplant-brownie
+//
+//  Created by Diogo Meneses de França on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var nomeTextField: UITextField!
+    @IBOutlet var felicidadeTextField: UITextField!
+
+    
+    @IBAction func adicionar(_ sender: Any) {
+        let nome = nomeTextField.text
+        let felicidade = felicidadeTextField.text
+        print("Comi \(nome) e fiquei com a felicidade \(felicidade)")
+        
+    }
+    
+
+}
+
